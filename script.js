2aa705249bdd1aae4b31d27f95be1eeba14f3569
Morgan <morgan.em.day@gmail.com>
2023-05-18 04:47:50 -0600

    Broken Script! ...But it's a start? :)

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //create object
 //changes based on input 
 
-const HTMLdisplay = document.getElementById('HTMLdisplay')
+const display = document.getElementById('display')
 
 const calculator = {
     displayValue: '0',  //default string value in calculator bar displayed is 0
@@ -10,4 +10,9 @@ const calculator = {
     operator: null,
   };
 
+  const updateDisplay = (a) => display.innerHTML = display;
 
+  const appendInput = (a) => calculator.displayValue += a.toString(); updateDisplay();
+
+
+  console.log(calculator.displayValue);
