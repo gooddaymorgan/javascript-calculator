0513ca45ef16c5094ae10d8d82da120a0b38a54e
Morgan <morgan.em.day@gmail.com>
2023-05-18 04:56:17 -0600

    Formatted Document + Removed Arrow Functions + New Function Testing

--- script.js
+++ script.js
@@ -5,14 +5,18 @@ const display = document.getElementById('display')
 
 const calculator = {
     displayValue: '0',  //default string value in calculator bar displayed is 0
-    firstOperand: null, 
-    waitingForSecondOperand: false, 
+    firstOperand: null,
+    waitingForSecondOperand: false,
     operator: null,
-  };
+};
 
-  const updateDisplay = (a) => display.innerHTML = display;
-
-  const appendInput = (a) => calculator.displayValue += a.toString(); updateDisplay();
+console.log("Before" + calculator.displayValue);
 
 
-  console.log(calculator.displayValue);
+const updateDisplay = (a) => display.innerHTML = calculator.displayValue;
+
+function appendInput(a) {
+    calculator.displayValue += a.toString();
+    updateDisplay();
+    console.log(calculator.displayValue);
+}
