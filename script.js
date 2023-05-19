cc62646d7b45f7aac34d60b7acd0398b9257ddec
Morgan <morgan.em.day@gmail.com>
2023-05-19 00:40:56 -0600

    Organizing Code

--- script.js
+++ script.js
@@ -1,44 +1,49 @@
-//create object
-//changes based on input 
-
-const display = document.getElementById('display')
-
 const calculator = {
-    displayValue: '',  //default string value in calculator bar displayed is 0
-    calculationValue: 0,
-    calculationArray: [],
-    firstOperand: null,
-    operatorArray: [],
+    displayValue: '',  //takes user input as strings to display in HTML
+    calculationValue: 0, //takes user input for actual calculation
+    calculationArray: [], //stores our final values to calculate
+    firstOperand: false, //if false add operator to string
+    operatorArray: [], //stores our operators
 };
 
 //updates our display
+const display = document.getElementById('display')
 const updateDisplay = () => display.innerHTML = calculator.displayValue;
 
+//---------------------------------------------------->
+
+//adds input from button press to our calculator object
 function appendInput(number) {
 
-    //adds value from caculation to HTML display
-    //creates visual reprsentation of math equation?? I think that's how you word it
+    //adds our input number to a string to be updated for our HTML
     calculator.displayValue += number.toString();
 
-    //creates display into first number in equation 
-    calculator.calculationValue = parseFloat(calculator.displayValue);
+    // adds our input number to our calculation value
+    calculator.calculationValue = parseFloat(calculator.displayValue); //expected output (ex 9999 or 1234) should continously add numbers to end
 
     updateDisplay();
-    console.log("This is my calculation value" + calculator.calculationValue );
 
+    //console.log("This is my calculation value" + calculator.calculationValue );
+
+    //TBA --- if has string? updated operand?
 }
+
+// pushes our variables in calculator object to array to store for final calculation
+function addArray() {
+    value = calculator.calculationValue;
+    calculator.calculationArray.push(value);
+}
+
+//adds operators to our calculator 
 function setOperator(operator) {
 
-    //if we don't have operator in the front append operator to display value & push final number in array to be calculated
-    if (calculator.firstOperand == null) {
-
-        value = calculator.calculationValue;
-        calculator.calculationArray.push(value); //ERROR RETURNS undefined??
-        calculator.displayValue += operator.toString();
-        calculator.firstOperand = true;
+    if (calculator.firstOperand == false) {
+        addArray(); //adds our number before operator into array for final calculation
+        calculator.displayValue += operator.toString(); //adds operator to our HTML
+        calculator.firstOperand = true; //sets operand to true to show that we have an operand in the string and cannot add another one until it is resolved
+        console.log("This is my array" + calculator.calculationArray)
     }
 
-    console.log("This is my array" + calculator.calculationArray)
     updateDisplay();
 }
 
@@ -47,17 +52,11 @@ function clearDisplay() {
     calculator.displayValue = '';
     calculator.calculationValue = 0,
     calculator.calculationArray = [];
-    calculator.firstOperand = null;
-
+    calculator.firstOperand = false;
     updateDisplay();
 }
 
 function calculate() {
-
-    console.log(calculator.displayValue)
-
-    finish = parseInt(calculator.displayValue)
-
     console.log(finish)
     updateDisplay();
 }
