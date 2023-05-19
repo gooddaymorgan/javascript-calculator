96034b60902f3d4051a5453d9f03f37b3d6fab41
Morgan <morgan.em.day@gmail.com>
2023-05-19 02:26:29 -0600

    Fixed The issue with the array only appending first # inputed

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const calculator = {
-    displayValue: '',  //takes user input as strings to display in HTML
-    calculationValue: 0, //takes user input for actual calculation
+    htmlString: '', //total expression
+    stringNumber: '', //meant to represent current number bieng inputed
+    currentInputNumber: 0, //takes user input for actual calculation
     calculationArray: [], //stores our final values to calculate
     canAddOperand: false, //if true add operator to string
     operatorArray: [], //stores our operators
@@ -8,31 +9,35 @@ const calculator = {
 
 //updates our display
 const display = document.getElementById('display')
-const updateDisplay = () => display.innerHTML = calculator.displayValue;
+const updateDisplay = () => display.innerHTML = calculator.htmlString;
 
 //---------------------------------------------------->
 
 //adds input from button press to our calculator object
 function appendInput(number) {
 
-    //adds our input number to a string to be updated for our HTML
-    calculator.displayValue += number.toString();
-
+    //adds our total input numberd to a string to be updated for our HTML
+    calculator.htmlString += number.toString();
+    calculator.stringNumber+= number.toString();
     // adds our input number to our calculation value
-    calculator.calculationValue = parseFloat(calculator.displayValue); //expected output (ex 9999 or 1234) should continously add numbers to end
-    calculator.canAddOperand = true;
-    //sets operand to false 
+    calculator.currentInputNumber = parseFloat(calculator.stringNumber); //expected output (ex 9999 or 1234) should continously add numbers to end
+    calculator.canAddOperand = true; //we have number inputed so now we can have operand
     updateDisplay();
 
-    //console.log("This is my calculation value" + calculator.calculationValue );
+    console.log("This is my calculation value" + calculator.currentInputNumber );
 
-    //TBA --- if has string? updated operand?
 }
+
+// function appendDecimal() {
+
+// }
 
 // pushes our variables in calculator object to array to store for final calculation
 function addArray() {
-    value = calculator.calculationValue;
-    calculator.calculationArray.push(value);
+
+    calculator.calculationArray.push(calculator.currentInputNumber);
+    calculator.currentInputNumber = 0;
+    calculator.stringNumber = ''; //sets to zero to restart the next number into array
 }
 
 //adds operators to our calculator 
@@ -40,19 +45,23 @@ function setOperator(operator) {
 
     if (calculator.canAddOperand == true) {
         addArray(); //adds our number before operator into array for final calculation
-        calculator.displayValue += operator.toString(); //adds operator to our HTML
+        calculator.htmlString += operator.toString(); //adds operator input to our HTML
+        calculator.operatorArray.push(operator.toString());
         calculator.canAddOperand = false; //sets operand to false to show that we have an operand in the string and cannot add another one until it is resolved
-        console.log("This is my array" + calculator.calculationArray)
+        console.log("This is my array" + calculator.calculationArray + "this is my operator " + calculator.operatorArray)
     }
 
     updateDisplay();
 }
 
+//reset our values
 function clearDisplay() {
-    //reset our values
-    calculator.displayValue = '';
-    calculator.calculationValue = 0,
+    
+    calculator.htmlString = '';
+    calculator.stringNumber= '',
+    calculator.currentInputNumber = 0,
     calculator.calculationArray = [];
+    calculator.operatorArray = [];
     calculator.canAddOperand = false;
     updateDisplay();
 }
